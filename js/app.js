bf7c6a05a64a6d67a44b103836732c23aaded9f5
axelbraunschweiger <axelbraunschweiger@gmail.com>
2018-10-09 15:41:23 -0500

    changed position instafeed, added bandinfo

--- js/app.js
+++ js/app.js
@@ -1,129 +1,129 @@
 $(document).ready(function () {
 
 
-$('#textsong2').hide();
-$('#textsong3').hide();
-$('#textsong4').hide();
-$('#textsong5').hide();
-$('#textsong6').hide();
-$('#textsong7').hide();
-$('#textsong8').hide();
-$('#song1').addClass('active');
-
-
-
-
-$('#song1').on('click', function () {
-    $('#textsong1').show('#textsong1');
-    $("li").removeClass('active');
+    $('#textsong2').hide();
+    $('#textsong3').hide();
+    $('#textsong4').hide();
+    $('#textsong5').hide();
+    $('#textsong6').hide();
+    $('#textsong7').hide();
+    $('#textsong8').hide();
     $('#song1').addClass('active');
-    $('#textsong2').hide();
-    $('#textsong3').hide();
-    $('#textsong4').hide();
-    $('#textsong5').hide();
-    $('#textsong6').hide();
-    $('#textsong7').hide();
-    $('#textsong8').hide();
-
-});
 
 
-$('#song2').on('click', function () {
-    $('#textsong2').show('#textsong2');
-    $('li').removeClass('active');
-    $('#song2').addClass('active');
-    $('#textsong1').hide();
-    $('#textsong3').hide();
-    $('#textsong4').hide();
-    $('#textsong5').hide();
-    $('#textsong6').hide();
-    $('#textsong7').hide();
-    $('#textsong8').hide();
-});
-
-$('#song3').on('click', function () {
-    $('#textsong3').show('#textsong3');
-    $('li').removeClass('active');
-    $('#song3').addClass('active');
-    $('#textsong1').hide();
-    $('#textsong2').hide();
-    $('#textsong4').hide();
-    $('#textsong5').hide();
-    $('#textsong6').hide();
-    $('#textsong7').hide();
-    $('#textsong8').hide();
-});
-
-$('#song4').on('click', function () {
-    $('#textsong4').show('#textsong4');
-    $('li').removeClass('active');
-    $('#song4').addClass('active');
-    $('#textsong1').hide();
-    $('#textsong2').hide();
-    $('#textsong3').hide();
-    $('#textsong5').hide();
-    $('#textsong6').hide();
-    $('#textsong7').hide();
-    $('#textsong8').hide();
-});
 
 
-$('#song5').on('click', function () {
-    $('#textsong5').show('#textsong5');
-    $('li').removeClass('active');
-    $('#song5').addClass('active');
+    $('#song1').on('click', function () {
+        $('#textsong1').show('#textsong1');
+        $("li").removeClass('active');
+        $('#song1').addClass('active');
+        $('#textsong2').hide();
+        $('#textsong3').hide();
+        $('#textsong4').hide();
+        $('#textsong5').hide();
+        $('#textsong6').hide();
+        $('#textsong7').hide();
+        $('#textsong8').hide();
 
-    $('#textsong1').hide();
-    $('#textsong2').hide();
-    $('#textsong3').hide();
-    $('#textsong4').hide();
-    $('#textsong6').hide();
-    $('#textsong7').hide();
-    $('#textsong8').hide();
-});
-
-$('#song6').on('click', function () {
-    $('#textsong6').show('#textsong6');
-    $('li').removeClass('active');
-    $('#song6').addClass('active');
-
-    $('#textsong1').hide();
-    $('#textsong2').hide();
-    $('#textsong3').hide();
-    $('#textsong4').hide();
-    $('#textsong5').hide();
-    $('#textsong7').hide();
-    $('#textsong8').hide();
-});
+    });
 
 
-$('#song7').on('click', function () {
-    $('#textsong7').show('#textsong7');
-    $('li').removeClass('active');
-    $('#song7').addClass('active');
+    $('#song2').on('click', function () {
+        $('#textsong2').show('#textsong2');
+        $('li').removeClass('active');
+        $('#song2').addClass('active');
+        $('#textsong1').hide();
+        $('#textsong3').hide();
+        $('#textsong4').hide();
+        $('#textsong5').hide();
+        $('#textsong6').hide();
+        $('#textsong7').hide();
+        $('#textsong8').hide();
+    });
 
-    $('#textsong1').hide();
-    $('#textsong2').hide();
-    $('#textsong3').hide();
-    $('#textsong4').hide();
-    $('#textsong5').hide();
-    $('#textsong6').hide();
-    $('#textsong8').hide();
-});
+    $('#song3').on('click', function () {
+        $('#textsong3').show('#textsong3');
+        $('li').removeClass('active');
+        $('#song3').addClass('active');
+        $('#textsong1').hide();
+        $('#textsong2').hide();
+        $('#textsong4').hide();
+        $('#textsong5').hide();
+        $('#textsong6').hide();
+        $('#textsong7').hide();
+        $('#textsong8').hide();
+    });
+
+    $('#song4').on('click', function () {
+        $('#textsong4').show('#textsong4');
+        $('li').removeClass('active');
+        $('#song4').addClass('active');
+        $('#textsong1').hide();
+        $('#textsong2').hide();
+        $('#textsong3').hide();
+        $('#textsong5').hide();
+        $('#textsong6').hide();
+        $('#textsong7').hide();
+        $('#textsong8').hide();
+    });
 
 
-$('#song8').on('click', function () {
-    $('#textsong8').show('#textsong8');
-    $('li').removeClass('active');
-    $('#song8').addClass('active');
-    $('#textsong1').hide();
-    $('#textsong2').hide();
-    $('#textsong3').hide();
-    $('#textsong4').hide();
-    $('#textsong5').hide();
-    $('#textsong7').hide();
-    $('#textsong6').hide();
-});
+    $('#song5').on('click', function () {
+        $('#textsong5').show('#textsong5');
+        $('li').removeClass('active');
+        $('#song5').addClass('active');
+
+        $('#textsong1').hide();
+        $('#textsong2').hide();
+        $('#textsong3').hide();
+        $('#textsong4').hide();
+        $('#textsong6').hide();
+        $('#textsong7').hide();
+        $('#textsong8').hide();
+    });
+
+    $('#song6').on('click', function () {
+        $('#textsong6').show('#textsong6');
+        $('li').removeClass('active');
+        $('#song6').addClass('active');
+
+        $('#textsong1').hide();
+        $('#textsong2').hide();
+        $('#textsong3').hide();
+        $('#textsong4').hide();
+        $('#textsong5').hide();
+        $('#textsong7').hide();
+        $('#textsong8').hide();
+    });
+
+
+    $('#song7').on('click', function () {
+        $('#textsong7').show('#textsong7');
+        $('li').removeClass('active');
+        $('#song7').addClass('active');
+
+        $('#textsong1').hide();
+        $('#textsong2').hide();
+        $('#textsong3').hide();
+        $('#textsong4').hide();
+        $('#textsong5').hide();
+        $('#textsong6').hide();
+        $('#textsong8').hide();
+    });
+
+
+    $('#song8').on('click', function () {
+        $('#textsong8').show('#textsong8');
+        $('li').removeClass('active');
+        $('#song8').addClass('active');
+        $('#textsong1').hide();
+        $('#textsong2').hide();
+        $('#textsong3').hide();
+        $('#textsong4').hide();
+        $('#textsong5').hide();
+        $('#textsong7').hide();
+        $('#textsong6').hide();
+    });
 
 
     $(document).on("scroll", onScroll);
@@ -162,6 +162,24 @@ function onScroll(event) {
 
 }
 
+
+$('#german').hide();
+
+$('.übersetzen').on('click', function () {
+
+    $('#english').hide();
+    $('#german').show();
+
+
+});
+
+$('.translate').on('click', function () {
+
+    $('#english').show();
+    $('#german').hide();
+
+
+});
 
 
 
--- js/custom.js
+++ js/custom.js
@@ -4,11 +4,11 @@ $(document).ready(function() {
 var userFeed = new Instafeed({
     get: 'user',
     userId: '1761627482',
-    limit :7,
+    limit :4,
     reslution: 'standard_resolution',
     accessToken: '1761627482.1677ed0.5c726cfb824247248a383f0f2ea84aba',
     sortBy: 'most-recent',
-    template: '<a href="{{link}}"><img src="{{image}}"/></a>',
+    template: '<a href="{{link}}" target="_blank"><img src="{{image}}" /></a>',
 });
 userFeed.run();
 
