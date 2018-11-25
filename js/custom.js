776e581e8f35f6d5e403fa72f54c1b26cf9f2182
axelbraunschweiger <axelbraunschweiger@gmail.com>
2018-11-25 18:46:36 -0500

    Implement fb-plugin and shop

--- js/custom.js
+++ js/custom.js
@@ -5,7 +5,7 @@ var userFeed = new Instafeed({
     get: 'user',
     userId: '1761627482',
     limit :3,
-    reslution: 'low_resolution',
+    reslution: 'standard_resolution',
     accessToken: '1761627482.1677ed0.5c726cfb824247248a383f0f2ea84aba',
     sortBy: 'most-recent',
     template: '<a href="{{link}}" target="_blank"><img src="{{image}}" /></a>',
